81fcaa619582d7778a82383baccb93dc09130f0d
JoseMartinez <mbianchijs@gmail.com>
2020-12-01 21:27:45 -0300

    Importacion e implementacion componente Result

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Result from './components/Result';
 import './App.css';
 
 const App = () => {
    return (<main className="react-calculator">
-       <div className="result">
-
-       </div>
+       <Result />
        <div className="numbers">
            <button>1</button>
            <button>2</button>
