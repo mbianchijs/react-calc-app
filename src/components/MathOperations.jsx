629846f60f6a1e8abe32bb70ac0594399a3fe6a8
JoseMartinez <mbianchijs@gmail.com>
2020-12-03 23:36:19 -0300

    Creacion Comp MathOperations e implementacion Proptypes

---
+++ src/components/MathOperations.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+const MathOperations = ({onClickOperation, onClickEqual}) => (
+    
+)
+
+MathOperations.PropTypes = {
+    onClickOperation: PropTypes.func.isRequired,
+    onClickEqual: PropTypes.func.isRequired
+}
+
+export default MathOperations
